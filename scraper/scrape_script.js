50e419bf44f0353f105d87dfdf46871150fb9d38
Ubuntu <ubuntu@ip-172-31-26-169.us-west-2.compute.internal>
2018-06-06 16:28:08 +0000

    Dev commit

--- scraper/scrape_script.js
+++ scraper/scrape_script.js
@@ -6,9 +6,8 @@ const { insertEtsyDataIntoDatabse, getMostRecentSales, shutDownDatabase, getURLs
 async function scrapeSaleHistory(page, activePageNumber, totalSales) {
   if (activePageNumber != 1) {
     const [response] = await Promise.all([page.waitForNavigation(), page.click(`a.page-${activePageNumber}`)]); // click on page
-  }
-
-  let sales = await page.$$eval('a.listing-link', el =>
+  }	
+let sales = await page.$$eval('a.listing-link', el =>
     el.map(e => {
       return { product_id: e.dataset.listingId, title: e.title };
     }),
@@ -48,10 +47,8 @@ function findSubarray(arr, subarr) {
 async function scrapeOrganization(page, url) {
   let name = await page.$eval('.shop-name-and-title-container h1', el => el.innerText);
   let numberOfSales = await page.$eval('.shop-sales', el => parseInt(el.innerText.split(' ')[0]));
-
   if (await page.$('.shop-sales a')) {
-    const [response] = await Promise.all([page.waitForNavigation(), page.click('.shop-sales a')]); // click on sales
-
+    await Promise.all([page.click('.shop-sales a'), page.waitForNavigation()]); // click on sales
     let allSales = [];
     let activePageNumber = 1;
     let previousSales = await getMostRecentSales(url);
@@ -60,14 +57,12 @@ async function scrapeOrganization(page, url) {
     let continueLoop = true;
 
     await page.setViewport({ width: 1200, height: 700 });
-
     do {
       allSales = await scrapeSaleHistory(page, activePageNumber, allSales);
       const result = searchForNewSales(allSales, previousSales); // has continue property and sales property.
       sales = result.sales;
       continueLoop = result.continue;
     } while ((await page.$(`a.page-${++activePageNumber}`)) && continueLoop);
-
     return { name, numberOfSales, publicSales: true, url, sales };
   } else return { name, numberOfSales, publicSales: false, url }; // If Private
 }
@@ -103,6 +98,8 @@ async function etsyScraper(urls) {
     console.log(`Scraping ${u}`);
 
     const data = await scrapeWebsite(browser, u);
+
+console.log(JSON.stringify(data, null, 4));
 
     console.log(`Successfully scraped ${u}`);
     console.log('Starting Database Insertion');
